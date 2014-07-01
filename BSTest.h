ee5c6b8fd00c4a728159c2ef7f388ced6ebe0673
adityabsoft <aditya.bsoft@gmail.com>
2014-07-01 11:05:07 +0530

    added a test class

---
+++ BSTest.h
@@ -0,0 +1,13 @@
+//
+//  BSTest.h
+//  Test Git
+//
+//  Created by Hadi Hatinoglu on 01/07/14.
+//  Copyright (c) 2014 Baraansoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSTest : NSObject
+
+@end
---
+++ BSTest.m
@@ -0,0 +1,13 @@
+//
+//  BSTest.m
+//  Test Git
+//
+//  Created by Hadi Hatinoglu on 01/07/14.
+//  Copyright (c) 2014 Baraansoft. All rights reserved.
+//
+
+#import "BSTest.h"
+
+@implementation BSTest
+
+@end
